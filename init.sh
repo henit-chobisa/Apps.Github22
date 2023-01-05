489627369cd41d8be3813fcbc2ea7e08a3d6b12b
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-05 15:29:06 +0000

    feat: Dry Run 2

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -H "Content-type:application/json" http://localhost:3000/api/v1/users.register -d '{ "username": "user0", "email": "a@b.com", "pass": "123456", "name": "user"}'
+sleep 7
+
+curl -H "Content-type:application/json" $(gp url 3000)/api/v1/users.register -d '{ "username": "user0", "email": "a@b.com", "pass": "123456", "name": "user"}'
