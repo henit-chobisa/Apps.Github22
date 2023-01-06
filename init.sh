f82dd78c2c05af67b7dd99aea1255175c8010d4a
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-06 04:19:02 +0000

    feat: GP Modified

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-data = $(curl -H "Content-type:application/json" http://localhost:3000/api/v1/users.register -d '{ "username": "user0", "email": "a@b.com", "pass": "123456", "name": "user"}')
+sleep 5
 
-echo $data
+curl -H "Content-type:application/json" http://localhost:3000/api/v1/users.register -d '{ "username": "user0", "email": "a@b.com", "pass": "123456", "name": "user"}'
+
+gp sync-done user-init
