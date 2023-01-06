05b45f6a5fadfb06ed17bb1188a690cca4f2e1ee
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-06 16:34:09 +0000

    feat: Showcase 14

--- init.go
+++ init.go
@@ -7,31 +7,46 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os/exec"
+	"strconv"
 	"time"
 )
 
 type User struct {
-	success  bool
-	username any
+	_id      string
+	status   string
+	active   string
+	name     string
+	username string
+}
+
+type Response struct {
+	success bool
+	user    User
 }
 
 func setUser() bool {
 	url := "http://localhost:3000/api/v1/users.register"
-	jsonData := map[string]string{`username`: "user5", `email`: "c@b.com", `pass`: "123456", `name`: "uer"}
-	fmt.Println(jsonData)
+	jsonData := map[string]string{`username`: "usd", `email`: "8@1.com", `pass`: "123456", `name`: "hiii"}
 	data, err := json.Marshal(jsonData)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(bytes.NewBuffer(data))
 	resp, error := http.Post(url, "application/json", bytes.NewBuffer(data))
 	if error != nil {
+		fmt.Println("Hello")
+		fmt.Println(error.Error())
 		return false
 	}
 	data, _ = ioutil.ReadAll(resp.Body)
-	var response User
+	var response map[string]interface{}
 	json.Unmarshal(data, &response)
-	return response.success
+	fmt.Println(bytes.NewBuffer(data))
+	fmt.Println()
+	boolValue, err := strconv.ParseBool(fmt.Sprintf("%v", response["success"]))
+	if err != nil {
+		return false
+	}
+	return boolValue
 }
 
 func main() {
