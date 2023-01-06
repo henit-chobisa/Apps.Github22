0c31482ab63091e278ffcfa8d00131dfd3a45f65
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-06 04:59:32 +0000

    feat: Showcase 11

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sleep 15
-
 curl -H "Content-type:application/json" http://localhost:3000/api/v1/users.register -d '{ "username": "user0", "email": "a@b.com", "pass": "123456", "name": "user"}'
 
 gp sync-done user-init
